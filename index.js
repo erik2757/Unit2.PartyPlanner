18f4804a6629510ad0d27adf499e0b20cdfc35ae
Erik Rodriguez <erikrodriguez@Eriks-Air.lan>
2024-02-14 15:04:35 -0600

    updated

---
+++ index.js
@@ -0,0 +1,51 @@
+//Setup
+
+const Base_URL = 'https://fsa-crud-2aa9294fe819.herokuapp.com/api/erik-rodriguez/events'
+
+let events = []
+
+const eventsContainer = document.getElementById('events')
+
+// Fetch Calls 
+
+async function getEvents(){ 
+    try{ 
+
+        const response = await fetch(`${Base_URL}`)
+        const json = await response.json()
+
+        return json.data
+
+    }catch(err){
+        console.error(err)
+    }
+}   
+
+//event listeners 
+
+
+//render functions 
+
+function renderEvents(){
+    const htmlEvents = events.map(eve => {
+        let div = document.createElement('div')
+        div.className = 'card'
+        div.innerHTML = `<h3>#${eve.id}</h3>
+                        <img src=${eve.imageUrl} />
+                         <h3>${eve.name}</h3> 
+                         <p>${eve.description}</p>`
+     
+    return div 
+    })
+
+eventsContainer.replaceChildren(...htmlEvents)    
+}
+
+async function startApp(){ 
+    events = await getEvents()
+
+    renderEvents()
+
+}
+
+startApp()
